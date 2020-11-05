0ed98fb461905546157e028b618e9ab7f702f35d
Ethan Passino <ethan.passino@fwcsstudents.org>
2020-11-05 14:01:20 -0500

    Finished TIY3

--- TIY3b.py
+++ TIY3b.py
@@ -1,1 +1,17 @@
+from TIY3 import Python_Repos
+import unittest
 
+
+class APICallTestCase(unittest.TestCase):
+
+    def test_status(self):
+        p = Python_Repos()
+        self.assertEqual(p.status_code, 200)
+
+    def test_repo_len(self):
+        p = Python_Repos()
+        self.assertEqual(p.get_num_items() > 10, True)
+
+
+if __name__ == '__main__':
+    unittest.main()
