4145042e11f01402cfe43d88e156f04b70e7dac1
Ethan Passino <ethan.passino@fwcsstudents.org>
2020-11-05 12:42:09 -0500

    Getting/looking at more information about what we pulled in.

--- python_repos.py
+++ python_repos.py
@@ -7,6 +7,14 @@ r = requests.get(url, headers=headers)
 print(f"Status code: {r.status_code}")
 # Store api response in a variable
 response_dict = r.json()
+print(f"Total repositories: {response_dict['total_count']}")
 
-# Process results.
-print(response_dict.keys())
+# Explore information about the repos
+repo_dicts = response_dict['items']
+print(f"Repos returned: {len(repo_dicts)}")
+
+# Examine the first repo
+repo_dict = repo_dicts[0]
+print(f"\nKeys: {len(repo_dict)}")
+for key in sorted(repo_dict.keys()):
+    print(key)
